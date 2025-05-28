a65cd10eca0bad717d101e5ac5fff210f1531c32
Juan Pablo Fernandez <jpprogrammer1@gmail.com>
2025-05-28 01:49:58 -0300

    Refactor CORS configuration and clean up code formatting in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Habilitar CORS
   app.enableCors({
-    origin: true,
+    origin: ['https://team-manager-ipbi.onrender.com', 'http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  
+
   // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('Planning Poker API')
@@ -20,16 +21,20 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  
+
   // Habilitar validación global
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
-  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.useWebSocketAdapter(new IoAdapter(app));
+
   // Iniciar el servidor
   const port = process.env.PORT || 3000;
   await app.listen(port);

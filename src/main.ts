a93bc0fabea170eccd03ed43293b0ab92594a78f
Juan Pablo Fernandez <jpprogrammer1@gmail.com>
2026-07-07 11:32:23 -0300

    fix: update CORS origin to new Railway production URL

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   // Habilitar CORS
   app.enableCors({
     origin: [
-      'https://team-manager-production-a807.up.railway.app',
+      'https://team-manager-production-a9d8.up.railway.app/',
       'http://localhost:5173',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
